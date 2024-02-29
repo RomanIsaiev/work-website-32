19286b7cfe61509208e3ad160f04f91c6463e867
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-02-29 19:01:16 +0200

    ok

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -8,7 +8,6 @@ import {
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const burger = document.querySelector("spin-burger");
-  // const closeMenuBtn = document.querySelector(".js-close-menu");
 
   const linksMenuButtons = document.querySelectorAll(".mobile-menu-links");
 
@@ -31,7 +30,6 @@ import {
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
-  // closeMenuBtn.addEventListener("click", toggleMenu);
 
   linksMenuButtons.forEach((item) =>
     item.addEventListener("click", toggleMenu)
