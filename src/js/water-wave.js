2506b083aac92f4e2959475b6a35564d017581b8
Roman Isaiev <roma.isaev.2000@gmail.com>
2024-02-29 21:12:34 +0200

    fix water-wave

--- src/js/water-wave.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function () {
-  var scrolled = window.scrollY;
-  var parallaxContent = document.querySelector(".parallax-wave");
-  parallaxContent.style.transform = "translateX(" + scrolled * 0.05 + "px)"; // Измените коэффициент (0.5) для управления скоростью параллакса
-});
